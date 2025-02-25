c63e95357670aa60a1187d55ed6cd1820959439a
mult1p11xr <f.kappatoni@gmail.com>
2025-02-25 23:48:00 +0100

    db conn 2nd point

--- src/db.php
+++ src/db.php
@@ -17,4 +17,6 @@ try {
     echo "Connection failed : " . $e->getMessage();
 }
 
-//check db connection, something is wrong with the values in db vs. dockerfile probably
+//1. check db connection, something is wrong with the values in db vs. dockerfile probably
+
+//2. go through the landing pages and make them connect - then troubleshoot and proceed with the backend
