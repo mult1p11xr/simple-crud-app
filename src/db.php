5d504f57a2b3f58803c359a41dce809fc9a39658
mult1p11xr <f.kappatoni@gmail.com>
2025-02-26 23:59:11 +0100

    commit2

--- src/db.php
+++ src/db.php
@@ -20,3 +20,5 @@ try {
 //1. check db connection, something is wrong with the values in db vs. dockerfile probably
 
 //2. go through the landing pages and make them connect - then troubleshoot and proceed with the backend
+
+//3. go to the gym loser
